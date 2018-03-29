f517a19927dcc5a1bf68765fcfd81cf6e33ebfea
Richard Moot <moot@richardmoot.com>
2018-03-29 06:49:00 -0700

    Add in catalog listing

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const SquareConnect = require('square-connect');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  (SquareConnect.ApiClient.instance)
+    .authentications["oauth2"]
+    .accessToken = functions.config().square.prod.access_token;
+
+  const catalogApi = new SquareConnect.CatalogApi();
+
+  catalogApi.listCatalog().then((catalog)=>{
+    console.log(catalog);
+    response.send(catalog.objects);
+    return;
+  }).catch((error)=>{
+    console.log(error);
+    response.send(error);
+  });
+
+  //response.send("Hello from Firebase!");
 });
