bfdd675193fb3dccfb35d6c46ce01062836d6400
Richard Moot <moot@squareup.com>
2018-03-30 11:48:47 -0700

    Finished login flow

--- functions/index.js
+++ functions/index.js
@@ -105,7 +105,7 @@ exports.callback = functions.https.onRequest((request, response) => {
                                   token.data.access_token)
               .then(firebaseToken => {
                 // Serve an HTML page that signs the user in and updates the user profile.
-                  response.send(signInFirebaseTemplate(firebaseToken));
+                  response.send(signInFirebaseTemplate(firebaseToken, user.data.email));
                   return;
               })
               .catch(error => {
@@ -133,17 +133,35 @@ exports.callback = functions.https.onRequest((request, response) => {
  * Generates the HTML template that signs the user in Firebase using the given token and closes the
  * popup.
  */
-function signInFirebaseTemplate(token) {
+function signInFirebaseTemplate(token, email) {
   return `
-    <script src="https://www.gstatic.com/firebasejs/3.6.0/firebase.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/4.12.0/firebase.js"></script>
     <script>
-      var token = '${token}';
+      // var token = '${token}';
       var config = {
         apiKey: '${functions.config().firebase.apiKey}'
       };
       var app = firebase.initializeApp(config);
-      app.auth().signInWithCustomToken(token).then(function() {
-        window.location.href = "https://checkout-now.firebaseapp.com/catalog";
+      var actionCodeSettings = {
+        // URL you want to redirect back to. The domain (www.example.com) for this URL
+        // must be whitelisted in the Firebase Console.
+        'url': "https://checkout-now.firebaseapp.com/", // Here we redirect back to this same page.
+        'handleCodeInApp': true // This must be true.
+        };
+      firebase.auth().sendSignInLinkToEmail("${email}", actionCodeSettings).then(function() {
+        // Save the email locally so you don’t need to ask the user for it again if they open
+        // the link on the same device.
+        window.localStorage.setItem('emailForSignIn', "${email}");
+        // The link was successfully sent. Inform the user.
+        alert('An email was sent to ' + "${email}" + '. Please use the link in the email to sign-in.');
+        window.close();
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        console.log(error);
+        // [END_EXCLUDE]
       });
     </script>`;
 }
