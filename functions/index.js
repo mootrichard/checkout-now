0a76bc79e208fc3ebbcf58f0d22ec1f99847df1c
Richard Moot <moot@squareup.com>
2018-03-30 13:35:22 -0700

    Fixed catalog to return dynamically based on user token

--- functions/index.js
+++ functions/index.js
@@ -46,20 +46,34 @@ function createFirebaseAccount(id, squareEmail, displayName, accessToken) {
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.catalog = functions.https.onRequest((request, response) => {
-  (SquareConnect.ApiClient.instance)
-    .authentications["oauth2"]
-    .accessToken = functions.config().square.prod.access_token;
+  admin.auth().verifyIdToken(request.body.idToken)
+    .then((decodedToken) => {
+      var uid = decodedToken.uid;
+      var squareToken = admin.database().ref(`/squareAccessToken/${uid}`).once('value').then((userToken) => {
 
-  const catalogApi = new SquareConnect.CatalogApi();
+        (SquareConnect.ApiClient.instance)
+        .authentications["oauth2"]
+          .accessToken = userToken.val();
 
-  catalogApi.listCatalog().then((catalog) => {
-    console.log(catalog);
-    response.send(catalog.objects);
-    return;
-  }).catch((error) => {
-    console.log(error);
-    response.send(error);
-  });
+        const catalogApi = new SquareConnect.CatalogApi();
+
+        catalogApi.listCatalog().then((catalog) => {
+          console.log(catalog);
+          response.send(catalog.objects);
+          return;
+        }).catch((error) => {
+          console.log(error);
+          response.send(error);
+        });
+
+        return;
+      }).catch(error => console.log(error));
+      return;
+    }).catch( (error) => {
+      // Handle error
+      response.send(error);
+      return;
+    });
 
 });
 
